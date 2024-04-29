0954fee600eae1ea2d9e1397e1b4fca07ceadc46
jvvassao <jvvassao@gmail.com>
2024-04-29 16:54:15 -0300

    primeirocommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-
+import { ItemComponent } from './item/item.component';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+declarations: [
+AppComponent,
+ItemComponent
+],
+imports: [
+BrowserModule,
+HttpClientModule
+],
+providers: [],
+bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
